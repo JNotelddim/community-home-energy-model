67e809741eba72c8144b246369da9dfcd393d327
JNotelddim <JNotelddim@users.noreply.github.com>
2023-12-04 17:20:49 -0800

    Add BCHydro cost calculation to output: demonstrate cost savings

--- src/main.py
+++ src/main.py
@@ -5,8 +5,8 @@ import pvlib
 from modeling.home import Home
 from modeling.home_weather import weather_for_home
 from modeling.energy_model import model_home_energy
-from modeling.home_energy_use_aggregate import get_annual_home_energy_use
-
+from modeling.home_energy_use_aggregate import get_annual_home_energy_use, get_monthly_home_energy_use
+from modeling.cost_estimate import estimate_cost_by_monthly_consumption_bc
 
 MODEL_YEAR=2022
 
@@ -59,15 +59,20 @@ if __name__ == '__main__':
             )
 
         homes_energy_usage={}
+        homes_monthly_energy_usage={}
         print("Annual totals for each home's energy use:")
         for home_id in models.keys():
+            print(f"Home {home_id}")
             home_annual_hvac_energy_use=get_annual_home_energy_use(models[home_id])
+            home_monthly_hvac_energy_use=get_monthly_home_energy_use(models[home_id])
             homes_energy_usage[home_id]=home_annual_hvac_energy_use
+            homes_monthly_energy_usage[home_id]=home_monthly_hvac_energy_use
             print(f'home {home_id} annual energy use: {math.floor(home_annual_hvac_energy_use)} kWh')
         
         print("Modeling again with home adjustments...")
         adjusted_models={}
         adjusted_homes_energy_usage={}
+        adjusted_homes_monthly_energy_usage={}
         for home in homes:
             print(f"Checking home {home.id} stats...")
             home_altered=False
@@ -86,7 +91,9 @@ if __name__ == '__main__':
                     window_irradiance
                 )
                 home_annual_hvac_energy_use=get_annual_home_energy_use(home_model)
+                home_monthly_hvac_energy_use=get_monthly_home_energy_use(models[home_id])
                 adjusted_homes_energy_usage[home.id]=home_annual_hvac_energy_use
+                adjusted_homes_monthly_energy_usage[home.id]=home_monthly_hvac_energy_use
                 print(f"Adjusted home energy use for home {home.id}: {math.floor(home_annual_hvac_energy_use)} kWh")
             
             adjusted_models[home.id] = home_model
@@ -96,13 +103,13 @@ if __name__ == '__main__':
         for home in homes:
             before_energy = homes_energy_usage[home.id]
             after_energy = adjusted_homes_energy_usage[home.id] if home.id in adjusted_homes_energy_usage.keys() else homes_energy_usage[home.id]
-            print(f"Home {home.id} [ before: {math.floor(before_energy)} kWh, after: {math.floor(after_energy)} kWh ] = { round((before_energy - after_energy) / before_energy * 100) }% reduction")
+            before_cost=estimate_cost_by_monthly_consumption_bc(homes_monthly_energy_usage[home.id])
+            after_cost=estimate_cost_by_monthly_consumption_bc(adjusted_homes_monthly_energy_usage[home.id]) if home.id in adjusted_homes_monthly_energy_usage.keys() else estimate_cost_by_monthly_consumption_bc(homes_monthly_energy_usage[home.id])
+            print(f"__ Home {home.id}")
+            print(f"CONSUMPTION: [ before: {math.floor(before_energy)} kWh, after: {math.floor(after_energy)} kWh ] = { round((before_energy - after_energy) / before_energy * 100) }% reduction")
+            print(f"COST: [ before: ${round(before_cost, 2)}, after: ${round(after_cost, 2)} ] = { round((before_cost - after_cost) / before_cost * 100) }% reduction")
         
         
-        # TODO:
-        # Convert energy savings to $ amount.
-
-
 
 
             
